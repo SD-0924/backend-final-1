94be6c113d57b1a8eaf359db25810429bd29e513
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-11-24 14:06:18 +0200

    bug fixed in models

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -1,46 +1,49 @@
-import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
-import sequelize from '../config/mySQLConf';
-import User from './User';
+import { Sequelize, DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/mySQLConf";
+import User from "./User";
 
 interface OrderAttributes {
-    id: string;
-    user_id: number;
-    status: string;
+  id: string;
+  user_id: string;
+  status: string;
 }
 
-interface OrderCreationAttributes extends Optional<OrderAttributes, 'id'> {}
+interface OrderCreationAttributes extends Optional<OrderAttributes, "id"> {}
 
-class Order extends Model<OrderAttributes, OrderCreationAttributes> implements OrderAttributes {
-    public id!: string;
-    public user_id!: number;
-    public status!: string;
+class Order
+  extends Model<OrderAttributes, OrderCreationAttributes>
+  implements OrderAttributes
+{
+  public id!: string;
+  public user_id!: string;
+  public status!: string;
 }
 
 Order.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: User,
-                key: 'id',
-            },
-        },
-        status: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        tableName: 'orders',
-        modelName: 'Order',
-    }
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "orders",
+    modelName: "Order",
+  }
 );
 
 export default Order;
