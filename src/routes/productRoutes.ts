78179e14f059c26c3458721741561318ba943f31
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-11-27 13:37:12 +0200

    get products + get prod by id + get prod rating

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+
 import {
+  getAllProducts,
+  getProductById,
   addProduct,
   updateProduct,
   deleteProduct,
+  getProductRatings,
   getNewArrivals,
 } from "../controllers/productController";
+
 import {
   validateAddProduct,
   validateUpdateProduct,
@@ -15,19 +20,27 @@ import { validateRequest } from "../middlewares/validateRequest";
 
 const router = Router();
 
+router.get("/api/products", validateRequest, getAllProducts);
+
+router.get("/api/products/:id", validateRequest, getProductById);
+
 router.post("/api/products", validateAddProduct, validateRequest, addProduct);
+
 router.put(
   "/api/products/:id",
   validateUpdateProduct,
   validateRequest,
   updateProduct
 );
+
 router.delete(
   "/api/products/:id",
   validateDeleteProduct,
   validateRequest,
   deleteProduct
 );
+
+router.get("/api/products/:id/ratings", validateRequest, getProductRatings);
 
 // Route for fetching new arrivals
 router.get(
