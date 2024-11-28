46a534912c4458329263bbbd7552ec626de5a25a
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-28 22:44:11 +0200

    adding brand routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import authRoutes from "./routes/userRoutes";
 import productRouts from "./routes/productRoutes";
 import couponRouts from "./routes/couponRoutes";
+import brandRoutes from "./routes/brandRoutes";
 import "./models/Associations";
 import errorHandlingMiddleware from "./errorHandling";
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(authRoutes);
 app.use(productRouts);
 app.use(couponRouts);
+app.use(brandRoutes);
 
 app.use(errorHandlingMiddleware);
 
