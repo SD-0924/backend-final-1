4aa01c6a59ab5a9d00ea1d1c4f46b1e452c69904
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-29 20:23:55 +0200

    fixing error in the getBrandById function

--- src/controllers/brandController.ts
+++ src/controllers/brandController.ts
@@ -23,16 +23,17 @@ export const createBrand = async (req: Request, res: Response) =>{
     }
 };
 
-export const getBrandById = async(req: Request, res: Response) =>{
+export const getBrandById = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
         const brand = await fetchBrandByIdService(id); 
 
         if (!brand) {
             res.status(404).json({ message: 'Brand not found.' });
+        }else{
+            res.status(200).json(brand); 
         }
-        res.status(200).json(brand); 
-
+        
     } catch (error) {
         res.status(500).json({ message: 'Internal server error.' });
     }
