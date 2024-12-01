6bc6df39f721c3b4d961d4de01f5842657529fd6
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-12-01 23:52:28 +0200

    adding the createBrand to controller

---
+++ src/controllers/brandController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import {
+    createBrandService,
+} from "../services/brandService";
+
+export const createBrand = async (req: Request, res: Response) => {
+    try {
+        // extract the product name from the body and the file to be uploaded to the firbase
+        const { name } = req.body;
+        const file = req.file;
+
+        if (!req.file) {
+        res.status(400).json({
+            success: false,
+            message: "A logo file is required for brand creation.",
+        });
+        }
+
+        const newBrand = await createBrandService(name, file!);
+
+        res.status(201).json({
+        success: true,
+        message: "Brand created successfully",
+        data: newBrand,
+        });
+    } catch (error) {
+        console.error("Error in Creating Brand.");
+
+        res.status(500).json({
+        success: false,
+        message: "Error creating brand",
+        });
+    }
+};
+
