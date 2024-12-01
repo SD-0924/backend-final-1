d8da77d9191460612ba2bddf02c2899aab60f15b
Taneem Jawabreh <tasneemkjawabreh@gmail.com>
2024-12-01 20:24:26 +0300

    update users endpoints

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { handleLogin, handleRegister, handleUpdateUser, handleDeleteUser, handleGetAllUsers, getUserByIdController } from '../controllers/userController';
-import { validateUserLogin, validateUserRegistration } from '../validations/userValidation';
+import { handleLogin, handleRegister, handleUpdateUser, handleDeleteUser, handleGetAllUsers, getUserByIdController, handleUpdateUseradress } from '../controllers/userController';
+import { validateUserLogin, validateUserRegistration, validateUserUpdate } from '../validations/userValidation';
 import { validateRequest } from '../middlewares/validateRequest';
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.post('/register', validateUserRegistration, validateRequest, handleRegist
 router.post('/login',validateUserLogin,validateRequest,handleLogin);
 router.get('/users', handleGetAllUsers);
 router.get('/users/:id', getUserByIdController);
-router.put('/:id', handleUpdateUser); // Update user by ID
+router.put('/:id', handleUpdateUseradress); // Update user by ID
+router.put("/users/:id", validateUserUpdate, handleUpdateUser);
 router.delete('/:id', handleDeleteUser); // Delete user by ID
 export default router;
