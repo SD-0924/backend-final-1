9a3e3a35605828b9c33072287f62c2502b8200de
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-12-01 19:17:56 +0200

    merge dev 2

--- src/server.ts
+++ src/server.ts
@@ -1,65 +1,32 @@
 import dotenv from "dotenv";
 import app from "./app";
-import sequelize from "./config/mySQLConf";  
+import sequelize from "./config/mySQLConf";
 import initializeDatabase from "./initializeDatabase";
-
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET!;
-const environment = process.env.NODE_ENV || "development";
-
-/*const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD||"";
-const DB_HOST = process.env.DB_HOST || "localhost";
-
-const sequelizeInitial = new Sequelize("", DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "mysql",
-  logging: false,
-});*/ //remove this comment if you want to initialize the database in your localhost
-
-/* const DB_PORT= process.env.DB_PORT;
-const DB_DATABASE=process.env.DB_DATABASE||"finalProj";
-const DB_USER = process.env.DB_USERNAME || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD||"";
-const DB_HOST = process.env.DB_HOST || "localhost";
-console.log(DB_DATABASE);
-const sequelizeInitial = new Sequelize('defaultdb', DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: 24723,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-  logging: console.log,
-});*/ //remove this comment if you want to initialize the database on Aiven but it is already initialized
 
 const startServer = async () => {
   try {
-    //  await initializeDatabase(sequelizeInitial, sequelize);
-    if (environment.toString().trim() !== "test") {
-      app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-        console.log("API Docs available at http://localhost:3000/api-docs");
-      });
-    }
+    await initializeDatabase(sequelize, sequelize);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+      console.log("API Docs available at http://localhost:3000/api-docs");
+    });
   } catch (error) {
     console.error("Error starting server:", error);
     process.exit(1);
   }
 
-
   process.on("SIGINT", async () => {
     console.log("Gracefully shutting down...");
-    await sequelize.close();     
+    await sequelize.close();
     console.log("Database connection closed.");
     process.exit(0);
   });
 };
-
 
 startServer();
