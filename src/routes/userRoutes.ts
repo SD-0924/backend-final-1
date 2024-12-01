7634b842e620cabc066686caeceafbde1467f836
Taneem Jawabreh <tasneemkjawabreh@gmail.com>
2024-12-01 10:35:35 +0300

    users endpoints without auth

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { handleLogin, handleRegister } from '../controllers/userController';
+import { handleLogin, handleRegister, handleUpdateUser, handleDeleteUser, handleGetAllUsers, getUserByIdController } from '../controllers/userController';
 import { validateUserLogin, validateUserRegistration } from '../validations/userValidation';
 import { validateRequest } from '../middlewares/validateRequest';
 const router = express.Router();
 
 router.post('/register', validateUserRegistration, validateRequest, handleRegister);
+
 router.post('/login',validateUserLogin,validateRequest,handleLogin);
+router.get('/users', handleGetAllUsers);
+router.get('/users/:id', getUserByIdController);
+router.put('/:id', handleUpdateUser); // Update user by ID
+router.delete('/:id', handleDeleteUser); // Delete user by ID
 export default router;
