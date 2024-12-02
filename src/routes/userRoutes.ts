82fc88d731a29e1e2a7ce5c51f25e2cbc5181ed1
Taneem Jawabreh <tasneemkjawabreh@gmail.com>
2024-12-02 23:20:35 +0300

    update users endpoints

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { handleLogin, handleRegister, handleUpdateUser, handleDeleteUser, handleGetAllUsers, getUserByIdController } from '../controllers/userController';
-import { validateUserLogin, validateUserRegistration } from '../validations/userValidation';
+import { handleLogin, handleRegister, handleUpdateUser, handleDeleteUser, handleGetAllUsers, getUserByIdController, handleUpdateUseradress } from '../controllers/userController';
+import { validateUserLogin, validateUserRegistration, validateUserUpdate } from '../validations/userValidation';
 import { validateRequest } from '../middlewares/validateRequest';
 const router = express.Router();
 
@@ -24,6 +24,7 @@ router.post('/register', validateUserRegistration, validateRequest, handleRegist
 router.post('/login',validateUserLogin,validateRequest,handleLogin);
 router.get('/users', handleGetAllUsers);
 router.get('/users/:id', getUserByIdController);
-router.put('/:id', handleUpdateUser); // Update user by ID
-router.delete('/:id', handleDeleteUser); // Delete user by ID
+router.put('users/address/:id', handleUpdateUseradress); // Update user by ID
+router.put("/users/:id", validateUserUpdate, handleUpdateUser);
+router.delete('/users/:id', handleDeleteUser); // Delete user by ID
 export default router;
