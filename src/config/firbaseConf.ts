719a44a0140251612ac8e5a58db4c44df5b37fd2
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-12-02 21:05:16 +0200

    Update firbaseConf.ts

--- src/config/firbaseConf.ts
+++ src/config/firbaseConf.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { initializeApp as adminInitializeApp, cert } from "firebase-admin/app";
 import * as admin from "firebase-admin";
 import * as path from "path";
+import fs from "fs";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -10,7 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // app for clinet-side
@@ -22,20 +23,27 @@ const serviceAccountConfig = process.env.SERVICE_ACCOUNT_PATH;
 if (!serviceAccountConfig) {
   throw new Error("SERVICE_ACCOUNT_PATH environment variable is not set.");
 }
-
+let serviceAccount;
+if (process.env.NODE_ENV !== "test") {
+  const serviceAccountPath = path.resolve(process.env.SERVICE_ACCOUNT_PATH!);
+  try {
+    serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+  } catch (error: any) {
+    throw new Error(`Failed to load service account JSON. Error: ${error.message}`);
+  }
+}
+ 
 // resolving the path for the service account file
-const serviceAccountPath = path.resolve(process.env.SERVICE_ACCOUNT_PATH!);
+/*const serviceAccountPath = path.resolve(process.env.SERVICE_ACCOUNT_PATH!);
 
 // importing the service account JSON
 let serviceAccount;
 try {
   serviceAccount = require(serviceAccountPath);
-} catch (error: any) {
-  throw new Error(
-    `Failed to load service account JSON. Path: ${serviceAccountPath}. Error: ${error.message}`
-  );
+} catch (error:any) {
+  throw new Error(`Failed to load service account JSON. Path: ${serviceAccountPath}. Error: ${error.message}`);
 }
-
+*/
 // admin sdk
 const adminApp = adminInitializeApp({
   credential: cert(serviceAccount),
