3b7c38c88b1d3c4308290283dc16dcbcc9a948bc
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-12-03 18:26:05 +0200

    updating product route - /api/products/by-brand/:brandId

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -28,13 +28,13 @@ import { authenticateJWT, isAdmin } from '../middlewares/authMiddleware';
 const router = Router();
 
 // Routes for products
-router.get("/api/products", authenticateJWT, validateGetAllProducts, getAllProducts);
+router.get("/api/products", validateGetAllProducts, getAllProducts);
 router.get("/api/products/:id", authenticateJWT, validateGetProductById, getProductById);
 router.post("/api/products", upload.single('image'), authenticateJWT, isAdmin, validateAddProduct, validateRequest, addProduct);
 router.put("/api/products/:id", upload.single('productImage'), authenticateJWT, isAdmin, validateUpdateProduct, updateProduct);
 router.delete("/api/products/:id", authenticateJWT, isAdmin, validateDeleteProduct, validateRequest, deleteProduct);
 router.get("/api/products/:id/ratings", validateGetProductRatings, getProductRatings);
 router.get("/api/products/new-arrivals", validateGetNewArrivals, getNewArrivals);
-router.get("/api/brands/:id/products", validateGetBrandProduct, validateGetAllProducts, getProductsByBrandController);
+router.get("/api/products/by-brand/:brandId", validateGetBrandProduct, validateGetAllProducts, getProductsByBrandController);
 
 export default router;
