1f1309eedc02bbaa631321fdcc9e0ddd302862eb
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-12-03 14:33:54 +0200

    T0-46 pro auth

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -56,7 +56,7 @@ export const handleLogin = async (
        res.status(401).json({ message: "Invalid email or password" });
     }
 
-    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ id: user.id , role: user.role}, JWT_SECRET, { expiresIn: "1h" });
 
     res.cookie("token", token, { httpOnly: true, secure: process.env.NODE_ENV === "production" });
 
