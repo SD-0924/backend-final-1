384b4e9472fcebe426082e0f3a7f54768218cae4
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-12-03 15:15:58 +0200

    T0-64 Brand Auth

--- src/routes/brandRoutes.ts
+++ src/routes/brandRoutes.ts
@@ -14,6 +14,7 @@ import {
 } from "../validations/brandValidation";
 import upload from "../middlewares/multerUpload";
 import { validateRequest } from "../middlewares/validateRequest";
+import { authenticateJWT, isAdmin } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
@@ -21,6 +22,7 @@ const router = express.Router();
 router.post(
   "/api/brands",
   upload.single("logo"),
+  authenticateJWT, isAdmin,
   validateAddBrand,
   validateRequest,
   createBrand
@@ -29,6 +31,7 @@ router.post(
 // get brand by ID endpoint
 router.get(
   "/api/brands/:id", 
+  authenticateJWT, 
   validateBrandId, 
   validateRequest, 
   getBrandById);
@@ -36,6 +39,7 @@ router.get(
 // get all brands
 router.get(
   "/api/brands", 
+  authenticateJWT, 
   validateGetAllBrands,
   validateRequest,
   getAllBrands
@@ -44,6 +48,7 @@ router.get(
 // delete brand by id
 router.delete(
   "/api/brands/:id",
+  authenticateJWT, isAdmin,
   validateBrandId,
   validateRequest,
   deleteBrandById
@@ -52,6 +57,7 @@ router.delete(
 // update brand
 router.put(
   "/api/brands/:id",
+  authenticateJWT, isAdmin,
   upload.single("logo"),
   validateUpdateBrand,
   validateRequest,
