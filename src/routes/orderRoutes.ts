6883c94ad883e4dfab319f4302cdd37938477486
TasneemJawabreh <78237721+TasneemJawabreh@users.noreply.github.com>
2024-12-05 00:52:37 +0300

    Delete src/routes/orderRoutes.ts

--- src/routes/orderRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUserOrders, placeOrder } from "../controllers/orderController";
-
-const router = Router();
-
-router.get("/:userId/orders", getUserOrders);
-router.post("/place-order", placeOrder);
-export default router;
