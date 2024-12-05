4bf599e537835787f7905744667e72dabb1f1897
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-12-05 19:27:55 +0200

    T0-96 validation

--- src/validations/ratingValidation.ts
+++ src/validations/ratingValidation.ts
@@ -1,6 +1,8 @@
 import { body, param, validationResult } from 'express-validator';
 
 export const validateRating = [
+  body('userId').isUUID().withMessage('Invalid product ID'),
+  body('productId').isUUID().withMessage('Invalid product ID'),
     body('ratingValue')
         .isFloat({ min: 1, max: 5 })
         .withMessage('Rating must be an integer between 1 and 5'),
