abd1abafa1f6929c0a1e506d221ae88d4bde1a73
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-12-06 16:06:11 +0200

    adding cartItem routes

--- src/routes/cartItemRoutes.ts
+++ src/routes/cartItemRoutes.ts
@@ -1,6 +1,19 @@
 import { Router } from "express";
+import { authenticateJWT } from "../middlewares/authMiddleware";
+import { validateAddToCart } from "../validations/cartItemValidation";
+import { validateRequest } from "../middlewares/validateRequest";
+import { addToCartController } from "../controllers/cartItemController";
+
 
 const router = Router();
+
+router.post("/api/carts",
+    // authenticateJWT, will add it later
+    validateAddToCart,
+    validateRequest,
+    addToCartController
+);
+
 
 
 export default router;
