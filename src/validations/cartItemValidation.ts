4b24729012dc2a01693f0eb7211253187dfceaef
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-12-07 00:38:18 +0200

    adding validation file

--- src/validations/cartItemValidation.ts
+++ src/validations/cartItemValidation.ts
@@ -20,3 +20,11 @@ export const validateuserId = [
     param('userId')
         .isUUID().withMessage("Invalid userId. It must be a valid UUID.")
 ]
+
+export const validateUpdateQuantity = [
+    param('cartId')
+        .isUUID().withMessage("Invalid userId. It must be a valid UUID."),
+    
+    body('quantity')
+        .isInt({ gt: 0 }).withMessage('Quantity is required and must be a positive integer')
+];
