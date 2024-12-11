36b54262b21671e6e0b03f75b12c02491bcd231a
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-12-11 08:42:48 +0200

    updating product test file to use the helper fucntion

--- src/tests/modelsTests/productModel.test.ts
+++ src/tests/modelsTests/productModel.test.ts
@@ -1,130 +1,10 @@
 import { Sequelize, DataTypes } from "sequelize";
 import Product from "../../models/Product";
-import Brand from "../../models/Brand";
-import Category from "../../models/Category";
-
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: ":memory:",
-  define: {
-    freezeTableName: true, // Prevent Sequelize from pluralizing table names
-  },
-});
-
-const MockBrand = sequelize.define(
-  "Brand",
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    logo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    tableName: "brands",
-  }
-);
-
-const MockCategory = sequelize.define(
-  "Category",
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-  },
-  {
-    tableName: "categories",
-  }
-);
-
-const MockProduct = sequelize.define(
-  "Product",
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-    },
-    stockQuantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    isLimitedEdition: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isFeatured: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    brandId: {
-      type: DataTypes.UUID,
-      allowNull: true,
-      references: {
-        model: Brand,
-        key: "id",
-      },
-    },
-    categoryId: {
-      type: DataTypes.UUID,
-      allowNull: true,
-      references: {
-        model: Category,
-        key: "id",
-      },
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    longDescription: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-  },
-  {
-    tableName: "products",
-    modelName: "Product",
-  }
-) as typeof Product;
+import User from "../../models/User";
+import Brand from "../../models/Brand"; 
+import Category from "../../models/Category"; 
+import { MockBrand, MockCategory, MockProduct} from "./mockDataHelper";
+import sequelize from "./mockDataHelper";
 
 describe("Product Model Test", () => {
   beforeAll(async () => {
@@ -135,17 +15,18 @@ describe("Product Model Test", () => {
       id: "a84c591a-6a2b-4800-997f-32d343094a",
       name: "Brand A",
       logo: "brand-image.png",
-    });
+    } as Brand);
     await MockCategory.create({
       id: "bc4fbd6e-37a1-403948e-86ba-695a56ee",
       name: "Category A",
-    });
+    } as Category);
 
     MockProduct.addHook("beforeCreate", (product) => {
       const productInstance = product as Product; // Cast to the Product type
       productInstance.isLimitedEdition = productInstance.stockQuantity < 20;
     });
   });
+  
   afterAll(async () => {
     await sequelize.close();
   });
