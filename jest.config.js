6c9e1a94cc41b9a21c848eb8906598be2d8ea2e8
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-12-12 14:03:10 +0200

    configer JEST with Winston

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   roots: ["<rootDir>/src/"],
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
