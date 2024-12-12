782e586c2e7b0f44a88a9c9d44803af4173b2811
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-12-12 19:24:10 +0200

    discount optimize

--- src/reposetories/discountRepository.ts
+++ src/reposetories/discountRepository.ts
@@ -13,36 +13,41 @@ export const createDiscount = async (data: any) => {
 };
 
 export const updateDiscount = async (id: string, data: any) => {
-    const discount = await Discount.findByPk(id);
-    if (discount) {
-        return await discount.update(data);
+    const discount = await Discount.update(data, {where:{id}});
+    if (!discount) {
+        throw new Error('Discount not found');
     }
-    throw new Error('Discount not found');
+    return discount;
 };
 
 export const deleteDiscount = async (id: string) => {
-    const discount = await Discount.findByPk(id);
+    const discount = await Discount.destroy({where:{id}});
     if (discount) {
-        return await discount.destroy();
+        throw new Error('Discount not found');
     }
-    throw new Error('Discount not found');
+    return discount;
 };
 
 export const getDiscountTimeRemainingById = async (discountId: string) => {
-    const discount = await Discount.findOne({
+     const discount = await Discount.findOne({
         where: { id: discountId },
+        attributes: ['endDate'], // Only fetch the necessary column
     });
 
     if (!discount) {
         throw new Error('Discount not found');
     }
 
-    const now = new Date();
-    if (discount.endDate < now) {
+    const now = Date.now(); // Use `Date.now()` for better performance
+    const endDate = new Date(discount.endDate).getTime();
+
+    // Check if the discount has expired
+    if (endDate < now) {
         return { remainingTime: 0, message: 'Discount has expired' };
     }
 
-    const remainingTime = discount.endDate.getTime() - now.getTime();
+    // Calculate remaining time
+    const remainingTime = endDate - now;
     return { remainingTime, message: 'Discount is still active' };
 };
 
